24f20ae532256b82829acbf94f92dfe9a40ae061
Sergey Leschinsky <leschinskysergey@gmail.com>
2021-08-08 13:22:38 +0300

    first branch to a CI/CD env
    Removed debug keystore file for jenkins
    
    Some minor changes to make unit tests work in jenkins
    
    more jenkins changes
    
    ignore -0
    
    ignore -1
    
    ignore -2

--- app/src/test/java/com/leschnitzky/dailyshiba/ExampleUnitTest.kt
+++ app/src/test/java/com/leschnitzky/dailyshiba/ExampleUnitTest.kt
@@ -1,17 +1,19 @@
 package com.leschnitzky.dailyshiba
 
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Test
+import kotlin.test.assertEquals
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@HiltAndroidTest
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
+    fun  addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
 }
